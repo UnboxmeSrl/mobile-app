8f12e61a0cfb5f23b8bc5b7edcd5a979ffdf55ed
Marcin Walendowski <kontakt@marcinwalendowski.pl>
2021-01-20 20:05:56 +0100

    feat: apple login improvements

--- src/components/AppleLogin/index.tsx
+++ src/components/AppleLogin/index.tsx
@@ -2,17 +2,18 @@ import React, { useEffect } from 'react'
 import { appleAuth } from '@invertase/react-native-apple-authentication'
 import { AppleLoginPresenter } from './AppleLoginPresenter'
 
-const onPress = async () => {
+const onPress = async (): Promise<any> => {
   const appleAuthRequestResponse = await appleAuth.performRequest({
     requestedOperation: appleAuth.Operation.LOGIN,
     requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
   })
-
+  console.log(appleAuthRequestResponse)
   // get current authentication state for user
   // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
   const credentialState = await appleAuth.getCredentialStateForUser(
     appleAuthRequestResponse.user,
   )
+  console.log(credentialState)
 
   // use credentialState response to ensure the user is authenticated
   if (credentialState === appleAuth.State.AUTHORIZED) {
